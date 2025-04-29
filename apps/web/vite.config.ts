bf30185362bf530ff485e40a5d3eddb0dc00e4b1
Joshua Chisolm <jkchisolm22@gmail.com>
2025-04-29 16:03:47 -0700

    update viteConfig to support env vars

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    define: {
+      "import.meta.env.VITE_SERVER_URL": process.env.VITE_SERVER_URL,
+      "import.meta.env.VITE_GOOGLE_CLIENT_ID":
+        process.env.VITE_GOOGLE_CLIENT_ID,
+    },
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  };
 });
