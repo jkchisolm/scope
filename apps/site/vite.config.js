dc7b1973a0b3da54ebf6a7c03362d4afd974d132
Joshua Chisolm <jkchisolm22@gmail.com>
2025-04-28 20:39:59 -0700

    update vite config js to add process.env?

--- apps/site/vite.config.js
+++ apps/site/vite.config.js
@@ -5,27 +5,35 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 import { preview } from "vite";
+import { loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+
+export default ({ mode }) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd()),
+  };
+  return defineConfig({
+    plugins: [
+      TanStackRouterVite({ autoCodeSplitting: true }),
+      viteReact(),
+      tailwindcss(),
+    ],
+    test: {
+      globals: true,
+      environment: "jsdom",
     },
-  },
-  preview: {
-    allowedHosts: ["scope.jkchisolm.com"],
-  },
-});
+    server: {
+      port: 3000,
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+    preview: {
+      allowedHosts: ["scope.jkchisolm.com"],
+    },
+  });
+};
