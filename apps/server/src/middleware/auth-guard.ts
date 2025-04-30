96569484521804aeb78b038e1eb478e3e65bf20c
Joshua Chisolm <jkchisolm22@gmail.com>
2025-04-30 16:11:12 -0700

    Remove old "site" directory, remove some console.logs in server

--- apps/server/src/middleware/auth-guard.ts
+++ apps/server/src/middleware/auth-guard.ts
@@ -1,9 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
 
 export const authGuard = (req: Request, res: Response, next: NextFunction) => {
-  console.log("cookies: ", req.cookies);
+  // console.log("cookies: ", req.cookies);
   if (req.cookies["better-auth.session_token"]) {
-    console.log("authenticated");
+    // console.log("authenticated");
     // If the user is authenticated, proceed to the next middleware
     next();
   } else {
